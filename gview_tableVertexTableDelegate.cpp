0248a581ffb38683ef6bc4087ab557382e88ac61
OmniCrow <4qui10nis@gmail.com>
2026-02-04 21:58:18 +0300

    Continue developing styled delegate for vertices table view.

--- gview_tableVertexTableDelegate.cpp
+++ gview_tableVertexTableDelegate.cpp
@@ -21,6 +21,7 @@ void IconDelegate::paint(QPainter* painter,
 {
     painter->save();
     QPixmap image;
+    QVariant data = index.data();
     ///!TODO get image form index
     painter->fillRect(option.rect,option.state & QStyle::State_Selected?
                           option.palette.highlight():
@@ -86,82 +87,39 @@ void NameDelegate::paint(QPainter* painter,
 {
     painter->save();
     painter->setRenderHint(QPainter::Antialiasing,true);
+    QVariant data = index.data(); ///!TODO get values from index' data
     QString f_name,l_name;
-    ///!TODO get values from index' data
+    QFont cur_font("Times",12, QFont::Normal);
+    QFontMetrics font_metrics(cur_font);
+    int max_str_p_size = std::max(font_metrics.horizontalAdvance(f_name),font_metrics.horizontalAdvance(l_name));
+    QRect text_rectangle1(0,0,max_str_p_size,pref_icon_size/2);
+    QRect text_rectangle2(0,pref_icon_size/2,max_str_p_size,pref_icon_size/2);
+    painter->setFont(cur_font);
+    painter->drawText(text_rectangle1,Qt::AlignCenter|Qt::AlignVCenter,f_name);
+    painter->drawText(text_rectangle2,Qt::AlignCenter|Qt::AlignVCenter,l_name);
+
+    /*
+        QString str("Short");
+        QString str1("Long Long String 2 ");
+        QFont cur_font("Times",12, QFont::Normal);
+        QFontMetrics font_metrics(cur_font);
+        int max_str_p_size = std::max(font_metrics.horizontalAdvance(str),font_metrics.horizontalAdvance(str1));
+        const int ellipce_size = 64;
+        const int space = 10;
+        QRect rectangle(0,0,ellipce_size+space*2+max_str_p_size,ellipce_size);
+        painter.setRenderHint(QPainter::Antialiasing,true);
+        painter.setBrush(QBrush(Qt::darkRed));
+        painter.setPen(QPen(Qt::black,2));
+        painter.drawRect(rectangle);
+        painter.drawEllipse(0,0,ellipce_size,ellipce_size);
+        QRect text_rectangle(ellipce_size+space,0,max_str_p_size,ellipce_size/2);
+        QRect text_rectangle2(ellipce_size+space,ellipce_size/2,max_str_p_size,ellipce_size/2);
+        painter.setFont(cur_font);
+        painter.drawText(text_rectangle,Qt::AlignCenter,str);
+        painter.drawText(text_rectangle2,Qt::AlignCenter,str1);
+*/
+
+
     painter->restore();
 }
 
-/*
-TableInfo::TableInfo(const QPixmap& image, const QString& info,
-                   const QString& additional_info)
-{
-    _image_ = image;
-    _info_ = info;
-    _additional_info_ = additional_info;
-    return;
-}
-
-bool TableInfo::setImage(const QPixmap& new_image)
-{
-    if(new_image.isNull())
-        return false;
-    _image_ = new_image;
-    return true;
-}
-bool TableInfo::setInfo(const QString& new_info)
-{
-    if(new_info.isEmpty())
-    {
-        return false;
-    }
-    _info_ = new_info;
-    return true;
-}
-
-bool TableInfo::setAdditionalInfo(const QString& new_add_info)
-{
-    _additional_info_ = new_add_info;
-    return true;
-}
-
-QPixmap TableInfo::getImage() const
-{
-    return _image_;
-}
-
-QString TableInfo::getInfo() const
-{
-    return _info_;
-}
-
-QString TableInfo::getAdditionalInfo() const
-{
-    return _additional_info_;
-}
-QSize TableInfo::sizeHint() const
-{
-    QFont local_font(QApplication::font());
-    QFontMetrics font_metrics(local_font);
-    QSize local_size(_image_.width()+font_metrics.horizontalAdvance(_info_),_image_.height());
-    return local_size;
-}
-
-void TableInfo::paint(QPainter* painter, const QRect& rect,
-           const QPalette& palette, Mode mode) const
-{
-
-}
-
-
-VertexTableDelegate::VertexTableDelegate()
-{
-
-}
-
-void VertexTableDelegate::paint(QPainter* painter,
-           const QStyleOptionViewItem& option,
-           const QModelIndex& index) const
-{
-
-}
-*/
