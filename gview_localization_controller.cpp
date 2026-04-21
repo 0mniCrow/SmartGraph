97e04a44d7177803f9c1d3e5e19939fde6f11709
OmniCrow <4qui10nis@gmail.com>
2026-04-21 21:31:35 +0300

    Add loading method and language change method for the translator class.

--- gview_localization_controller.cpp
+++ gview_localization_controller.cpp
@@ -14,8 +14,82 @@ bool GviewLangControl::canBeTranslated(const QString& class_name) const
             (class_name == QPushButton::staticMetaObject.className())||
             (class_name == QAction::staticMetaObject.className())||
             (class_name == QMenu::staticMetaObject.className())||
-            (class_name == QGroupBox::staticMetaObject.className()));
+            (class_name == QGroupBox::staticMetaObject.className())||
+           (class_name == QCheckBox::staticMetaObject.className())||
+           (class_name == QRadioButton::staticMetaObject.className()));
 
+}
+
+bool GviewLangControl::setTranslation(QObject *cur_obj, const QString& text, const QString& tooltip)
+{
+    if(!cur_obj)
+    {
+        return false;
+    }
+    QString class_name(cur_obj->metaObject()->className());
+
+    if(class_name == QLabel::staticMetaObject.className())
+    {
+        QLabel* elem = qobject_cast<QLabel*>(cur_obj);
+        elem->setText(text);
+    }
+    else if(class_name == QLineEdit::staticMetaObject.className())
+    {
+        QLineEdit* elem = qobject_cast<QLineEdit*>(cur_obj);
+        elem->setPlaceholderText(text);
+    }
+    else if(class_name == QTextEdit::staticMetaObject.className())
+    {
+        QTextEdit* elem = qobject_cast<QTextEdit*>(cur_obj);
+        elem->setPlaceholderText(text);
+    }
+    else if(class_name == QPushButton::staticMetaObject.className())
+    {
+        QPushButton* elem = qobject_cast<QPushButton*>(cur_obj);
+        elem->setText(text);
+    }
+    else if(class_name == QAction::staticMetaObject.className())
+    {
+        QAction* elem = qobject_cast<QAction*>(cur_obj);
+        elem->setText(text);
+        if(!tooltip.isEmpty())
+        {
+            elem->setToolTip(tooltip);
+        }
+    }
+    else if(class_name == QMenu::staticMetaObject.className())
+    {
+        QMenu* elem = qobject_cast<QMenu*>(cur_obj);
+        elem->setTitle(text);
+    }
+    else if(class_name == QGroupBox::staticMetaObject.className())
+    {
+        QGroupBox* elem = qobject_cast<QGroupBox*>(cur_obj);
+        elem->setTitle(text);
+    }
+    else if(class_name == QCheckBox::staticMetaObject.className())
+    {
+        QCheckBox* elem = qobject_cast<QCheckBox*>(cur_obj);
+        elem->setText(text);
+    }
+    else if(class_name == QRadioButton::staticMetaObject.className())
+    {
+        QRadioButton* elem = qobject_cast<QRadioButton*>(cur_obj);
+        elem->setText(text);
+    }
+    else
+    {
+        return false;
+    }
+    if(!tooltip.isEmpty())
+    {
+        QWidget* elem = qobject_cast<QWidget*>(cur_obj);
+        if(elem)
+        {
+            elem->setToolTip(tooltip);
+        }
+    }
+    return true;
 }
 
 void GviewLangControl::loadObject(QObject* cur_elem, QMap<QString,GViewTranslObj>& cur_dict)
@@ -51,6 +125,7 @@ bool GviewLangControl::loadWindow(QWidget* window)
     }
     QMap<QString,GViewTranslObj> dictionary;
     loadObject(window,dictionary);
+    _windows_.insert(window,dictionary);
     return true;
 }
 
@@ -71,9 +146,34 @@ bool GviewLangControl::loadTextTranslations(QWidget* window,
     }
     return true;
 }
-const QSet<QString> &GviewLangControl::translations() const
+const QSet<QString>& GviewLangControl::translations() const
 {
     return _translations_;
+}
+
+const QMap<QWidget*,QMap<QString,GViewTranslObj>>& GviewLangControl::getTranslatableObjectMap()const
+{
+    return _windows_;
+}
+
+QMap<QString,QMap<QString,QString>> GviewLangControl::getObjectMap() const
+{
+    QMap<QString,QMap<QString,QString>> windows;
+    auto window_iter = _windows_.cbegin();
+    while(window_iter!=_windows_.cend())
+    {
+        QMap<QString,QString> objects;
+        auto object_iter = window_iter->cbegin();
+        while(object_iter!= window_iter->cend())
+        {
+            objects.insert(object_iter->_obj_pointer_->objectName(),
+                           object_iter->_obj_pointer_->metaObject()->className());
+            ++object_iter;
+        }
+        windows.insert(window_iter.key()->objectName(),objects);
+        ++window_iter;
+    }
+    return windows;
 }
 
 bool GviewLangControl::changeLanguage(const QString& lang)
@@ -81,6 +181,19 @@ bool GviewLangControl::changeLanguage(const QString& lang)
     if(!_translations_.contains(lang))
     {
         return false;
+    }
+    auto window_iter = _windows_.begin();
+    while(window_iter!=_windows_.end())
+    {
+        auto obj_iter = window_iter.value().begin();
+        while(obj_iter!= window_iter.value().end())
+        {
+            setTranslation(obj_iter->_obj_pointer_,
+                           obj_iter->_obj_text_translations_.value(lang),
+                           obj_iter->_obj_tooltip_translation_.value(lang));
+            ++obj_iter;
+        }
+        ++window_iter;
     }
     return true;
 }
--- gview_localization_controller.h
+++ gview_localization_controller.h
@@ -12,6 +12,8 @@
 #include <QGroupBox>
 #include <QMenu>
 #include <QAction>
+#include <QCheckBox>
+#include <QRadioButton>
 
 struct GViewTranslObj
 {
@@ -29,11 +31,14 @@ private:
     QSet<QString> _translations_;
     void loadObject(QObject* next_elem, QMap<QString,GViewTranslObj>& cur_dict);
     bool canBeTranslated(const QString& class_name) const;
+    bool setTranslation(QObject* cur_obj, const QString& text, const QString& tooltip = QString());
 public:
     explicit GviewLangControl(QObject* tata = nullptr);
     bool loadWindow(QWidget* window);
     bool loadTextTranslations(QWidget* window, const QMap<QString,GViewTranslObj>& window_dict);
     const QSet<QString>&  translations() const;
+    const QMap<QWidget*,QMap<QString,GViewTranslObj>>& getTranslatableObjectMap()const;
+    QMap<QString,QMap<QString,QString>> getObjectMap() const;
     bool changeLanguage(const QString& lang);
 public slots:
     void objectAboutToBeDestroyed(QWidget* window);
