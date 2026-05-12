478a4e6888f41696f24ed7a3faef0aff15e3e0c9
OmniCrow <4qui10nis@gmail.com>
2026-05-12 19:28:42 +0300

    Add object for time data control.

--- widgets/gview_timeline_tool.cpp
+++ widgets/gview_timeline_tool.cpp
@@ -1,5 +1,73 @@
 #include "gview_timeline_tool.h"
 
+///____________AbstractTimeLine_________________
+AbstractTimeLine::AbstractTimeLine():
+    _start_time_(Q_UINT64_C(0)),
+    _end_time_(Q_UINT64_C(0)),
+    _cur_time_(Q_UINT64_C(0))
+{
+    return;
+}
+
+bool AbstractTimeLine::addStaticTimeSpace(const QString& name,
+                        const QString& child_name,
+                        quint64 numb_of_child_timespaces,
+                        const QString& parent_name)
+{
+
+}
+
+bool AbstractTimeLine::addVariedTimeSpace(const QString& name,
+                        const QString& child_name,
+                        const QList<QPair<QString,quint64>>& varied_names_and_num,
+                        const QString& parent_name)
+{
+
+}
+
+bool AbstractTimeLine::addMultiChildTimeSpace(const QString& name,
+                            const QMap<QString,quint64>& children_map,
+                            const QString& parent_name)
+{
+
+}
+
+bool AbstractTimeLine::isExist(const QString& name) const
+{
+
+}
+
+quint64 AbstractTimeLine::currentTime(const QString& scale_name) const
+{
+
+}
+
+bool AbstractTimeLine::addTime(quint64 time, const QString& scale)
+{
+
+}
+bool AbstractTimeLine::substractTime(quint64 time, const QString& scale)
+{
+
+}
+
+AbstractTimeLine& AbstractTimeLine::operator+=(qint64 time)
+{
+
+}
+
+AbstractTimeLine& AbstractTimeLine::operator-=(qint64 time)
+{
+
+}
+
+const AbstractTimeSpace *  AbstractTimeLine::currentScaledObject(const QString& scale_name)
+{
+
+}
+
+///____________GViewTimeTool____________________
+///
 GViewTimeTool::GViewTimeTool(QObject *parent)
     : QObject{parent}
 {
--- widgets/gview_timeline_tool.h
+++ widgets/gview_timeline_tool.h
@@ -9,23 +9,66 @@
 #include <QGridLayout>
 #include <QMap>
 
-struct AbstractTimeSpace
+struct AbstractTimeSpace                            //Абстрактны тып для адзінкі часовага прамежку
 {
     QString _name_;                                 //Імя бягучага прамежку
-    QString _child_name_;                           //Імя спадкавага прамежку
-    int _child_space_num_;                          //Колькасць спадкавых прамежкаў у бягучым
+    QString _parent_name_;                          //Імя наступнага статычнага прамежку
+    enum timespace_type{TS_NoType = 0};
+    virtual char type() const {return TS_NoType;}
 };
 
+struct StaticTimeSpace:public AbstractTimeSpace     //Статычны тып часу для нязьменных аб'ектаў, як гадзіны (60 хвілін)
+{                                                   //ці дні (24 гадзіны)
+    QString _child_name_;                           //Імя спадкавага прамежку (хвіліна для гадзіны, гадзіна для дня)
+    quint64 _child_num_;                                //Колькасць спадкавых прамежкаў у бягучым прамежку. (60 хвілін у адной гадзіне і г.д.)
+    enum timespace_type{TS_StaticType = 1};
+    char type() const override {return TS_StaticType;}
+};
+
+struct VariedTimeSpace:public AbstractTimeSpace     //Зьменны тып часу для аб'ектаў як месяц (30 ці 31 дзень)
+{
+    QString _child_name_;
+    QList<QPair<QString,quint64>> _varied_name_num_;   //Упарадкаваны спіс імёнаў падтыпу часу й прамежкаў у часе
+    enum timespace_type{TS_VariedType = 2};
+    char type() const override {return TS_VariedType;}
+};
+
+struct MultiChildTimeSpace:public AbstractTimeSpace     //Тып часу, які мае некалькі відаў спадкавых прамежкаў (год мае месяцы і дні адначасова)
+{
+    QMap<QString,quint64>  _child_names_num_;               //Назва спадкавага прамежку і яго колькасць.
+    enum timespace_type{TS_MultiChild = 3};
+    char type() const override {return TS_MultiChild;}
+};
 
 class AbstractTimeLine
 {
 private:
-    unsigned long long _start_time_;
-    unsigned long long _end_time;
-    AbstractTimeSpace _minimal_space_;
+    quint64 _start_time_;
+    quint64 _end_time_;
+    quint64 _cur_time_;
+    QString _minimal_space_name_;
+    QMap<QString,AbstractTimeSpace*> _time_types_;
 
 public:
     explicit AbstractTimeLine();
+    bool addStaticTimeSpace(const QString& name,
+                            const QString& child_name = QString(),
+                            quint64 numb_of_child_timespaces = 0,
+                            const QString& parent_name = QString());
+    bool addVariedTimeSpace(const QString& name,
+                            const QString& child_name,
+                            const QList<QPair<QString,quint64>>& varied_names_and_num,
+                            const QString& parent_name = QString());
+    bool addMultiChildTimeSpace(const QString& name,
+                                const QMap<QString,quint64>& children_map,
+                                const QString& parent_name = QString());
+    bool isExist(const QString& name) const;
+    quint64 currentTime(const QString& scale_name) const;
+    bool addTime(quint64 time, const QString& scale = QString());
+    bool substractTime(quint64 time, const QString& scale = QString());
+    AbstractTimeLine& operator+=(qint64 time);
+    AbstractTimeLine& operator-=(qint64 time);
+    const AbstractTimeSpace *  currentScaledObject(const QString& scale_name = QString());
 
 };
 
